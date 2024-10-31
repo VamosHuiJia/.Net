f9bb21fb30243676c83341a1194b4162c3100c09
Huy <giahuy20092004@gmail.com>
2024-10-31 16:53:45 +0700

    Add files via upload

---
+++ BaiKT_NguyenGiaHuy/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaiKT_NguyenGiaHuy
+{
+
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.productListView = new System.Windows.Forms.ListView();
+            this.cartListView = new System.Windows.Forms.ListView();
+            this.lblTotalAmount = new System.Windows.Forms.Label();
+            this.lblTotalQuantity = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(9, 457);
+            this.addButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(114, 30);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Thêm vào giỏ hàng";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 457);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Xóa khỏi giỏ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(580, 457);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chi tiết";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // productListView
+            // 
+            this.productListView.HideSelection = false;
+            this.productListView.Location = new System.Drawing.Point(9, 116);
+            this.productListView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.productListView.Name = "productListView";
+            this.productListView.Size = new System.Drawing.Size(349, 328);
+            this.productListView.TabIndex = 5;
+            this.productListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // cartListView
+            // 
+            this.cartListView.HideSelection = false;
+            this.cartListView.Location = new System.Drawing.Point(377, 116);
+            this.cartListView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cartListView.Name = "cartListView";
+            this.cartListView.Size = new System.Drawing.Size(374, 328);
+            this.cartListView.TabIndex = 6;
+            this.cartListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblTotalAmount
+            // 
+            this.lblTotalAmount.AutoSize = true;
+            this.lblTotalAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalAmount.Location = new System.Drawing.Point(475, 446);
+            this.lblTotalAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalAmount.Name = "lblTotalAmount";
+            this.lblTotalAmount.Size = new System.Drawing.Size(92, 17);
+            this.lblTotalAmount.TabIndex = 7;
+            this.lblTotalAmount.Text = "Total Amount";
+            this.lblTotalAmount.Click += new System.EventHandler(this.lblTotalAmount_Click);
+            // 
+            // lblTotalQuantity
+            // 
+            this.lblTotalQuantity.AutoSize = true;
+            this.lblTotalQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalQuantity.Location = new System.Drawing.Point(374, 446);
+            this.lblTotalQuantity.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalQuantity.Name = "lblTotalQuantity";
+            this.lblTotalQuantity.Size = new System.Drawing.Size(97, 17);
+            this.lblTotalQuantity.TabIndex = 8;
+            this.lblTotalQuantity.Text = "Total Quantity";
+            this.lblTotalQuantity.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "CỬA HÀNG BÁN ĐỒ ĐIỆN TỬ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "SẢN PHẨM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(525, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "GIỎ HÀNG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 498);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotalQuantity);
+            this.Controls.Add(this.lblTotalAmount);
+            this.Controls.Add(this.cartListView);
+            this.Controls.Add(this.productListView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView productListView;
+        private System.Windows.Forms.ListView cartListView;
+        private System.Windows.Forms.Label lblTotalAmount;
+        private System.Windows.Forms.Label lblTotalQuantity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+
+
+}
+
